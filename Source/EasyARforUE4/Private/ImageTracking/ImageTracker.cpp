6d624bf61c75caa1dcbf0fa5a45fdd548a843b13
qiusongtao <qiusongtao121731@pwrd.com>
2022-01-07 19:41:25 +0800

    Fixed Rotation Bug

--- Source/EasyARforUE4/Private/ImageTracking/ImageTracker.cpp
+++ Source/EasyARforUE4/Private/ImageTracking/ImageTracker.cpp
@@ -1,5 +1,4 @@
 ﻿#include "ImageTracking/ImageTracker.h"
-
 #include "Components/SceneCaptureComponent2D.h"
 
 static FMatrix MatrixConverter(easyar::Matrix44F& MatEasyAR);
@@ -8,15 +7,15 @@ static FTransform GetTransformFromMat44F(easyar::Matrix44F& MatEasyAR, FVector S
 UImageTrackers::UImageTrackers()
 {
 	PrimaryComponentTick.bCanEverTick = true;
-	PrimaryComponentTick.TickGroup = TG_PostPhysics;
+	// PrimaryComponentTick.TickGroup = TG_PostPhysics;
 	_imageTracker = std::make_unique<ImageTrackerWrapper>();
-	_motionTracker = std::make_unique<MotionTrakerWrapper>();
+	// _motionTracker = std::make_unique<MotionTrakerWrapper>();
 }
 
 UImageTrackers::~UImageTrackers()
 {
 	_imageTracker.release();
-	_motionTracker.release();
+	// _motionTracker.release();
 	delete CameraRenderer;
 }
 
@@ -36,51 +35,14 @@ void UImageTrackers::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	Timer += DeltaTime;
 
-	if (Timer >= 1. / FrameRate)
-	{
-		Timer -= 1. / FrameRate;
-		_motionTracker->render();
-		auto CurrentFrame = _motionTracker->cameraFrame;
-		auto Buffer = CurrentFrame->inputFrame()->image()->buffer();
-		CameraRenderer->Render(Buffer->data());
-		
-		auto easyarProjection = CurrentFrame->inputFrame()->cameraParameters()->projection(0.01, 1000., (float)OutRT->SizeX / (float)OutRT->SizeY, 0, true, false);
-		FReversedZPerspectiveMatrix PerspectiveMatrix = FReversedZPerspectiveMatrix(45, (float)OutRT->SizeX, (float)OutRT->SizeY, 0.01);
-	
-		PerspectiveMatrix.M[0][0] = easyarProjection.data[1];
-		PerspectiveMatrix.M[1][1] = -easyarProjection.data[4];
-		PerspectiveMatrix.M[3][2] = -easyarProjection.data[11];
-		
-		SceneCaptureA->CustomProjectionMatrix = PerspectiveMatrix;
-		SceneCaptureB->CustomProjectionMatrix = PerspectiveMatrix;
-		
-		auto easyarCamTrans = _motionTracker->cameraTransform;
-
-		StaticMeshComponent->SetStaticMesh(ImageTargetsCollection[FString("namecard.jpg")].Mesh);
-
-		FTransform TmpMeshTransform = FTransform(GetTransformFromMat44F(
-			easyarCamTrans,
-			FVector(1)));
-		StaticMeshComponent->SetWorldTransform(FTransform(FRotator(0, 0, 0), FVector(0), FVector(0.2)));
-		
-		SceneCaptureA->SetWorldTransform(TmpMeshTransform);
-		SceneCaptureB->SetWorldTransform(TmpMeshTransform);
-		
-		GEngine->AddOnScreenDebugMessage(
-			0, 1.0f, FColor::Green,
-			FString::Printf(TEXT("%s\n"), *FString(SceneCaptureA->GetComponentLocation().ToString())));
-		GEngine->AddOnScreenDebugMessage(
-			1, 1.0f, FColor::Green,
-			FString::Printf(TEXT("%s\n"), *FString(StaticMeshComponent->GetComponentLocation().ToString())));
-	}
-	
 	// if (Timer >= 1. / FrameRate)
 	// {
 	// 	Timer -= 1. / FrameRate;
-	// 	_imageTracker->perFrame();
-	// 	auto CurrentFrame = _imageTracker->cameraFrame;
+	// 	_motionTracker->render();
+	// 	auto CurrentFrame = _motionTracker->cameraFrame;
 	// 	auto Buffer = CurrentFrame->inputFrame()->image()->buffer();
-	//
+	// 	CameraRenderer->Render(Buffer->data());
+	// 	
 	// 	auto easyarProjection = CurrentFrame->inputFrame()->cameraParameters()->projection(0.01, 1000., (float)OutRT->SizeX / (float)OutRT->SizeY, 0, true, false);
 	// 	FReversedZPerspectiveMatrix PerspectiveMatrix = FReversedZPerspectiveMatrix(45, (float)OutRT->SizeX, (float)OutRT->SizeY, 0.01);
 	//
@@ -88,70 +50,94 @@ void UImageTrackers::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 	// 	PerspectiveMatrix.M[1][1] = -easyarProjection.data[4];
 	// 	PerspectiveMatrix.M[3][2] = -easyarProjection.data[11];
 	// 	
-	// 	SceneCaptureA->CustomProjectionMatrix = PerspectiveMatrix;
-	// 	SceneCaptureB->CustomProjectionMatrix = PerspectiveMatrix;
+	// 	// SceneCaptureA->CustomProjectionMatrix = PerspectiveMatrix;
+	// 	// SceneCaptureB->CustomProjectionMatrix = PerspectiveMatrix;
 	// 	
-	// 	// GEngine->AddOnScreenDebugMessage(
-	// 	// 	1, 1.0f, FColor::Green,
-	// 	// 	FString::Printf(TEXT("%s\n"), *FString(PerspectiveMatrix.ToString())));
+	// 	auto easyarCamTrans = _motionTracker->cameraTransform;
+	//
+	// 	StaticMeshComponent->SetStaticMesh(ImageTargetsCollection[FString("namecard.jpg")].Mesh);
+	//
+	// 	FTransform TmpMeshTransform = FTransform(GetTransformFromMat44F(
+	// 		easyarCamTrans,
+	// 		FVector(1)));
+	// 	StaticMeshComponent->SetWorldTransform(FTransform(FRotator(0, 0, 0), FVector(30, 0, -20), FVector(2)));
 	// 	
-	// 	CameraRenderer->Render(Buffer->data());
-	//
-	// 	if (_imageTracker->TrackTargets.size() != 0)
-	// 	{
-	// 		for (auto target : _imageTracker->TrackTargets)
-	// 		{
-	// 			
-	// 			if (ImageTargetsCollection.Contains(FString(target.second->name().c_str())))
-	// 			{
-	// 				// GEngine->AddOnScreenDebugMessage(
-	// 				// 	0, 1.0f, FColor::Green,
-	// 				// 	FString::Printf(TEXT("Found Target (%s): %d\n"), *FString(target.second->name().c_str()), target.second->runtimeID()));
-	// 				// GEngine->AddOnScreenDebugMessage(
-	// 				// 	0, 1.0f, FColor::Green,
-	// 				// 	FString::Printf(TEXT("Target: %s (%d)\n %f %f %f %f\n %f %f %f %f\n %f %f %f %f\n %f %f %f %f\n\n"),
-	// 				// 	*FString(target.second->name().c_str()), target.second->runtimeID(),
-	// 				// 	_imageTracker->targetPose.data[0], _imageTracker->targetPose.data[1], _imageTracker->targetPose.data[2], _imageTracker->targetPose.data[3],
-	// 				// 	_imageTracker->targetPose.data[4], _imageTracker->targetPose.data[5], _imageTracker->targetPose.data[6], _imageTracker->targetPose.data[7],
-	// 				// 	_imageTracker->targetPose.data[8], _imageTracker->targetPose.data[9], _imageTracker->targetPose.data[10], _imageTracker->targetPose.data[11],
-	// 				// 	_imageTracker->targetPose.data[12], _imageTracker->targetPose.data[13], _imageTracker->targetPose.data[14], _imageTracker->targetPose.data[15])
-	// 				// 	);
-	// 				
-	// 				FTransform TmpMeshTransform = FTransform(GetTransformFromMat44F(
-	// 					_imageTracker->targetPose,
-	// 					ImageTargetsCollection[FString(target.second->name().c_str())].MeshTransform.GetScale3D()));
-	// 				
-	// 				StaticMeshComponent->SetStaticMesh(ImageTargetsCollection[FString(target.second->name().c_str())].Mesh);
-	// 				// StaticMeshComponent->SetRelativeTransform(FTransform::Identity);
-	// 				// ViewInfo.Location = TmpMeshTransform.GetTranslation();
-	// 				// ViewInfo.Rotation = TmpMeshTransform.Rotator();
-	// 				// SceneCaptureA->SetCameraView(ViewInfo);
-	// 				// SceneCaptureB->SetCameraView(ViewInfo);
-	//
-	// 				// GEngine->AddOnScreenDebugMessage(
-	// 				// 	0, 1.0f, FColor::Green,
-	// 				// 	FString::Printf(TEXT("%s\n"), *FString(TmpMeshTransform.Rotator().ToString())));
-	// 				
-	// 				StaticMeshComponent->SetRelativeTransform(TmpMeshTransform);
-	// 				StaticMeshComponent->AddLocalRotation(FRotator(90, 0, 0));
-	// 				// StaticMeshComponent->AddLocalTransform(ImageTargetsCollection[FString(target.second->name().c_str())].MeshTransform);
-	// 			}
-	// 			// if (StaticMeshComponent->GetStaticMesh() != nullptr)
-	// 			// 	StaticMeshComponent->SetStaticMesh(nullptr);
-	// 		}
-	// 	}
+	// 	SceneCaptureA->SetWorldTransform(TmpMeshTransform);
+	// 	SceneCaptureB->SetWorldTransform(TmpMeshTransform);
 	// }
+	
+	if (Timer >= 1. / FrameRate)
+	{
+		Timer -= 1. / FrameRate;
+		_imageTracker->perFrame();
+		auto CurrentFrame = _imageTracker->cameraFrame;
+		auto Buffer = CurrentFrame->inputFrame()->image()->buffer();
+
+		float FOVX = FMath::DegreesToRadians(70.0f);
+		float FOVY = FMath::DegreesToRadians(30.0f);
+		auto easyarProjection = CurrentFrame->inputFrame()->cameraParameters()->projection(0.01, 1000., (float)OutRT->SizeX / (float)OutRT->SizeY, 0, true, false);
+		FReversedZPerspectiveMatrix PerspectiveMatrix = FReversedZPerspectiveMatrix(FOVX, FOVY, 1, 1, 0.01, 10000);
+	
+		// PerspectiveMatrix.M[0][0] = easyarProjection.data[1];
+		// PerspectiveMatrix.M[0][1] = easyarProjection.data[0];
+		// PerspectiveMatrix.M[1][0] = easyarProjection.data[5];
+		// PerspectiveMatrix.M[1][1] = -easyarProjection.data[4];
+		// PerspectiveMatrix.M[2][2] = -easyarProjection.data[11];
+	
+		
+		SceneCaptureA->CustomProjectionMatrix = PerspectiveMatrix;
+		SceneCaptureB->CustomProjectionMatrix = PerspectiveMatrix;
+
+		GEngine->AddOnScreenDebugMessage(
+			0, 1.0f, FColor::Green,
+			FString::Printf(TEXT("%s\n"), *FString(PerspectiveMatrix.ToString())));
+
+		GEngine->AddOnScreenDebugMessage(
+			1, 1.0f, FColor::Green,
+			FString::Printf(TEXT("%s\n"), *FString(MatrixConverter(easyarProjection).ToString())));
+		
+		CameraRenderer->Render(Buffer->data());
+	
+		if (_imageTracker->TrackTargets.size() != 0)
+		{
+			for (auto target : _imageTracker->TrackTargets)
+			{
+				
+				if (ImageTargetsCollection.Contains(FString(target.second->name().c_str())))
+				{
+					// GEngine->AddOnScreenDebugMessage(
+					// 	0, 1.0f, FColor::Green,
+					// 	FString::Printf(TEXT("Found Target (%s): %d\n"), *FString(target.second->name().c_str()), target.second->runtimeID()));
+					
+					float ImageTargetSize = target.second->scale() * 100.;
+					float targetScale = ImageTargetSize / 18.;
+					FTransform TmpMeshTransform = FTransform(GetTransformFromMat44F(
+						_imageTracker->targetPose,
+						FVector(targetScale)
+						// ImageTargetsCollection[FString(target.second->name().c_str())].MeshTransform.GetScale3D()
+						));
+					StaticMeshComponent->SetStaticMesh(ImageTargetsCollection[FString(target.second->name().c_str())].Mesh);
+					SceneCaptureA->SetWorldTransform(TmpMeshTransform.Inverse());
+					SceneCaptureB->SetWorldTransform(TmpMeshTransform.Inverse());
+				}
+			}
+		}
+		else 
+		{
+			StaticMeshComponent->SetStaticMesh(nullptr);
+		}
+	}
 }
 
 void UImageTrackers::Initialize()
 {
 	SceneCaptureA->bUseCustomProjectionMatrix = true;
 	SceneCaptureB->bUseCustomProjectionMatrix = true;
-	
+	StaticMeshComponent->AddLocalRotation(FRotator(90, 0, 0));
 	CameraRenderer = new FCameraRenderer(Width, Height, OutRT);
 	
 	_imageTracker->initialize();
-	_motionTracker->initialize();
+	// _motionTracker->initialize();
 
 	for (auto target : ImageTargetsCollection) 
 	{
@@ -162,13 +148,13 @@ void UImageTrackers::Initialize()
 void UImageTrackers::Start()
 {
 	_imageTracker->start();
-	_motionTracker->start();
+	// _motionTracker->start();
 }
 
 void UImageTrackers::Stop()
 {
 	_imageTracker->stop();
-	_motionTracker->stop();
+	// _motionTracker->stop();
 }
 
 FString UImageTrackers::GetImagePath(FString& ImageName)
@@ -197,6 +183,7 @@ FMatrix MatrixConverter(easyar::Matrix44F& MatEasyAR)
 
 static FTransform GetTransformFromMat44F(easyar::Matrix44F& MatEasyAR, FVector Scale)
 {
+	float rad = 0.0174532925199444; // FQuat needs Radians. So degree * Pi/180 | Pi/180 = 0.0174532...
 	FMatrix Sz = FMatrix::Identity;
 	Sz.M[2][2] = -1;
 
@@ -208,21 +195,20 @@ static FTransform GetTransformFromMat44F(easyar::Matrix44F& MatEasyAR, FVector S
 	
 	FVector Translation = FVector(MatEasyAR.data[3], MatEasyAR.data[7], MatEasyAR.data[11]);
 	
-	Rotation = Sz * Rotation * Sz;
-	// Rotation.M[2][2] *= -1;
+	// Rotation = Sz * Rotation * Sz;		// 通过四元数转换，不需要这一步
 	Translation = Sz.TransformPosition(Translation);
 	
 	Translation *= 100.;
 	Translation = FVector(Translation[2], Translation[0], Translation[1]);
 
-	FRotator Rotator = FQuat(Rotation).Rotator();
+	FRotator Rotator = Rotation.Rotator();
 	
-	float Pitch = Rotator.Roll;				// X->Y
-	float Yaw = Rotator.Pitch;				// Y->Z
-	float Roll = Rotator.Yaw;				// Z->X
-	Rotator = FRotator(Pitch, Yaw, Roll);
-	Rotator.Pitch *= -1.;
+	FQuat qx(FVector(1, 0, 0), -Rotator.Yaw * rad);
+	FQuat qz(FVector(0, 0, 1), -Rotator.Pitch * rad);
+	FQuat qy(FVector(0, 1, 0), -Rotator.Roll * rad);
 	
-	FTransform Result = FTransform(Rotator, Translation, Scale);
+	FQuat Quat = qy * qz * qx;
+	
+	FTransform Result = FTransform(FRotator(Quat), Translation, Scale);
 	return Result;
 }
